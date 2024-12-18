d976f85f69b3c09eb51e741a2f19dbd87e953bf5
iceray-Li <108821718+iceray00@users.noreply.github.com>
2024-12-18 20:37:11 +0800

    Update Node Influence running script

--- node_influence/run.sh
+++ node_influence/run.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-echo "python3 script.py -n 5 -m node_influence/PageRank.py -p results"
+
+echo "@@@@@ RUNNING CommunityDetectionCentrality @@@@@"
+python3 script.py -n 5 -m node_influence/CommunityDetectionCentrality.py -p results
+
+echo "@@@@@@ RUNNING PageRank @@@@@@"
+python3 script.py -n 5 -m node_influence/PageRank.py -p results
+
+echo "@@@@@@ RUNNING WaveletTransform @@@@@@"
 python3 script.py -n 5 -m node_influence/WaveletTransform.py -p results
 
-echo "python3 script.py -n 5 -m node_influence/StandardDeviation.py -p results"
+echo "@@@@@@ RUNNING StandardDeviation @@@@@@"
 python3 script.py -n 5 -m node_influence/StandardDeviation.py -p results
 
-echo "python3 script.py -n 5 -m node_influence/SGT_BetweennessCentrality.py -p results"
+echo "@@@@@@ RUNNING SGT_BetweennessCentrality @@@@@@"
 python3 script.py -n 5 -m node_influence/SGT_BetweennessCentrality.py -p results
 
-echo "python3 script.py -n 5 -m node_influence/SGT_ClosenessCentrality.py -p results"
+echo "@@@@@@ RUNNING SGT_ClosenessCentrality @@@@@@"
 python3 script.py -n 5 -m node_influence/SGT_ClosenessCentrality.py -p results
 
-echo "python3 script.py -n 5 -m node_influence/SGT_Laplacian.py -p results"
+echo "@@@@@@ RUNNING SGT_Laplacian @@@@@@"
 python3 script.py -n 5 -m node_influence/SGT_Laplacian.py -p results
 
