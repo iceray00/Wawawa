5bdb49f5212ec1281bb9173e4e2223a850ad5340
iceray-Li <108821718+iceray00@users.noreply.github.com>
2024-12-18 20:57:33 +0800

    Update

--- ablation/run.sh
+++ ablation/run.sh
@@ -2,16 +2,16 @@
 
 
 echo "@@@@ RUNNING: NIEN.py @@@@"
-python3 script_full.py -n 5 -m NIEN.py -p results
+python3 script_full.py -n 5 -m NIEN.py -p results_ablation
 
 echo "@@@@ RUNNING: NIEN_FixedEncoding.py @@@@"
-python3 script_full.py -n 5 -m NIEN_FixedEncoding.py -p results
+python3 script_full.py -n 5 -m NIEN_FixedEncoding.py -p results_ablation
 
 echo "@@@@ RUNNING: NIEN_SingleScale.py @@@@"
-python3 script_full.py -n 5 -m NIEN_SingleScale.py -p results
+python3 script_full.py -n 5 -m NIEN_SingleScale.py -p results_ablation
 
 echo "@@@@ RUNNING: NIEN_WithoutCausalMask.py @@@@"
-python3 script_full.py -n 5 -m NIEN_WithoutCausalMask.py -p results
+python3 script_full.py -n 5 -m NIEN_WithoutCausalMask.py -p results_ablation
 
 echo "@@@@ RUNNING: NIEN_WithoutNodeInfluence.py @@@@"
-python3 script_full.py -n 5 -m NIEN_WithoutNodeInfluence.py -p results
+python3 script_full.py -n 5 -m NIEN_WithoutNodeInfluence.py -p results_ablation
